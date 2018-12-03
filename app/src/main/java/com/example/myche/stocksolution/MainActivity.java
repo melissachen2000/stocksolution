5671d963d3f947d9a643d4a43138fcafaaa44938
melissachen2000 <31445526+melissachen2000@users.noreply.github.com>
2018-12-03 01:14:42 -0600

    connected main and news page through button click

--- app/src/main/java/com/example/myche/stocksolution/MainActivity.java
+++ app/src/main/java/com/example/myche/stocksolution/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.myche.stocksolution;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button[] newsbuttons = new Button[20];
+
+        newsbuttons[0] = (Button) findViewById(R.id.getnews01);
+        newsbuttons[1] = (Button) findViewById(R.id.getnews02);
+        newsbuttons[2] = (Button) findViewById(R.id.getnews03);
+        newsbuttons[3] = (Button) findViewById(R.id.getnews04);
+        newsbuttons[4] = (Button) findViewById(R.id.getnews05);
+        newsbuttons[5] = (Button) findViewById(R.id.getnews06);
+        newsbuttons[6] = (Button) findViewById(R.id.getnews07);
+        newsbuttons[7] = (Button) findViewById(R.id.getnews08);
+        newsbuttons[8] = (Button) findViewById(R.id.getnews09);
+        newsbuttons[9] = (Button) findViewById(R.id.getnews10);
+        newsbuttons[10] = (Button) findViewById(R.id.getnews11);
+        newsbuttons[11] = (Button) findViewById(R.id.getnews12);
+        newsbuttons[12] = (Button) findViewById(R.id.getnews13);
+        newsbuttons[13] = (Button) findViewById(R.id.getnews14);
+        newsbuttons[14] = (Button) findViewById(R.id.getnews15);
+        newsbuttons[15] = (Button) findViewById(R.id.getnews16);
+        newsbuttons[16] = (Button) findViewById(R.id.getnews17);
+        newsbuttons[17] = (Button) findViewById(R.id.getnews18);
+        newsbuttons[18] = (Button) findViewById(R.id.getnews19);
+        newsbuttons[19] = (Button) findViewById(R.id.getnews20);
+
+        for (int i = 0; i < newsbuttons.length; i++) {
+            newsbuttons[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(MainActivity.this, NewsActivity.class));
+                }
+            });
+        }
     }
+
 }
