3178adc4b3ad0274ddd2954872a98907ba761615
melissachen2000 <31445526+melissachen2000@users.noreply.github.com>
2018-12-04 21:44:04 -0600

    changes text when refresh button is hit or activity is changed

--- app/src/main/java/com/example/myche/stocksolution/MainActivity.java
+++ app/src/main/java/com/example/myche/stocksolution/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button[] newsbuttons = new Button[20];
-
+        final Button[] newsbuttons = new Button[20];
         newsbuttons[0] = (Button) findViewById(R.id.getnews01);
         newsbuttons[1] = (Button) findViewById(R.id.getnews02);
         newsbuttons[2] = (Button) findViewById(R.id.getnews03);
@@ -46,14 +48,116 @@ public class MainActivity extends AppCompatActivity {
             });
         }
 
+        final TextView[] tickers = new TextView[20];
+        tickers[0] = (TextView) findViewById(R.id.ticker01);
+        tickers[1] = (TextView) findViewById(R.id.ticker02);
+        tickers[2] = (TextView) findViewById(R.id.ticker03);
+        tickers[3] = (TextView) findViewById(R.id.ticker04);
+        tickers[4] = (TextView) findViewById(R.id.ticker05);
+        tickers[5] = (TextView) findViewById(R.id.ticker06);
+        tickers[6] = (TextView) findViewById(R.id.ticker07);
+        tickers[7] = (TextView) findViewById(R.id.ticker08);
+        tickers[8] = (TextView) findViewById(R.id.ticker09);
+        tickers[9] = (TextView) findViewById(R.id.ticker10);
+        tickers[10] = (TextView) findViewById(R.id.ticker11);
+        tickers[11] = (TextView) findViewById(R.id.ticker12);
+        tickers[12] = (TextView) findViewById(R.id.ticker13);
+        tickers[13] = (TextView) findViewById(R.id.ticker14);
+        tickers[14] = (TextView) findViewById(R.id.ticker15);
+        tickers[15] = (TextView) findViewById(R.id.ticker16);
+        tickers[16] = (TextView) findViewById(R.id.ticker17);
+        tickers[17] = (TextView) findViewById(R.id.ticker18);
+        tickers[18] = (TextView) findViewById(R.id.ticker19);
+        tickers[19] = (TextView) findViewById(R.id.ticker20);
+
+        for (TextView ticker:tickers) {
+            ticker.setText("NULL");
+        }
+
+        final TextView[] prices = new TextView[20];
+        prices[0] = (TextView) findViewById(R.id.price01);
+        prices[1] = (TextView) findViewById(R.id.price02);
+        prices[2] = (TextView) findViewById(R.id.price03);
+        prices[3] = (TextView) findViewById(R.id.price04);
+        prices[4] = (TextView) findViewById(R.id.price05);
+        prices[5] = (TextView) findViewById(R.id.price06);
+        prices[6] = (TextView) findViewById(R.id.price07);
+        prices[7] = (TextView) findViewById(R.id.price08);
+        prices[8] = (TextView) findViewById(R.id.price09);
+        prices[9] = (TextView) findViewById(R.id.price10);
+        prices[10] = (TextView) findViewById(R.id.price11);
+        prices[11] = (TextView) findViewById(R.id.price12);
+        prices[12] = (TextView) findViewById(R.id.price13);
+        prices[13] = (TextView) findViewById(R.id.price14);
+        prices[14] = (TextView) findViewById(R.id.price15);
+        prices[15] = (TextView) findViewById(R.id.price16);
+        prices[16] = (TextView) findViewById(R.id.price17);
+        prices[17] = (TextView) findViewById(R.id.price18);
+        prices[18] = (TextView) findViewById(R.id.price19);
+        prices[19] = (TextView) findViewById(R.id.price20);
+
+        for (TextView price:prices) {
+            price.setText("$0.00");
+        }
+
+        final TextView[] names = new TextView[20];
+        names[0] = (TextView) findViewById(R.id.company01);
+        names[1] = (TextView) findViewById(R.id.company02);
+        names[2] = (TextView) findViewById(R.id.company03);
+        names[3] = (TextView) findViewById(R.id.company04);
+        names[4] = (TextView) findViewById(R.id.company05);
+        names[5] = (TextView) findViewById(R.id.company06);
+        names[6] = (TextView) findViewById(R.id.company07);
+        names[7] = (TextView) findViewById(R.id.company08);
+        names[8] = (TextView) findViewById(R.id.company09);
+        names[9] = (TextView) findViewById(R.id.company10);
+        names[10] = (TextView) findViewById(R.id.company11);
+        names[11] = (TextView) findViewById(R.id.company12);
+        names[12] = (TextView) findViewById(R.id.company13);
+        names[13] = (TextView) findViewById(R.id.company14);
+        names[14] = (TextView) findViewById(R.id.company15);
+        names[15] = (TextView) findViewById(R.id.company16);
+        names[16] = (TextView) findViewById(R.id.company17);
+        names[17] = (TextView) findViewById(R.id.company18);
+        names[18] = (TextView) findViewById(R.id.company19);
+        names[19] = (TextView) findViewById(R.id.company20);
+
+        for (TextView name:names) {
+            name.setText("No company name");
+        }
+
         Button newsButton = (Button) findViewById(R.id.button);
         final EditText maxGetter = (EditText) findViewById(R.id.editText);
 
         newsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String maximumStockPrice = maxGetter.getText().toString().trim();
-                int maxPrice = Integer.parseInt(maximumStockPrice);
+                int maxPrice;
+                try {
+                    String maximumStockPrice = maxGetter.getText().toString().trim();
+                    maxPrice = Integer.parseInt(maximumStockPrice);
+                } catch (Exception e) {
+                    maxPrice = 100;
+                    maxGetter.setText(Integer.toString(maxPrice));
+                }
+
+                int count = 1;
+                for (TextView ticker:tickers) {
+                    ticker.setText("TICKER" + count);
+                    count++;
+                }
+
+                count = 1;
+                for (TextView price:prices) {
+                    price.setText("$" + count + ".00");
+                    count++;
+                }
+
+                count = 1;
+                for (TextView name:names) {
+                    name.setText("Company " + count);
+                    count++;
+                }
             }
         });
     }
--- app/src/main/java/com/example/myche/stocksolution/NewsActivity.java
+++ app/src/main/java/com/example/myche/stocksolution/NewsActivity.java
@@ -2,6 +2,9 @@ package com.example.myche.stocksolution;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class NewsActivity extends AppCompatActivity {
 
@@ -9,5 +12,44 @@ public class NewsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.news_page);
+
+        final TextView[] headlines = new TextView[5];
+        headlines[0] = (TextView) findViewById(R.id.headline01);
+        headlines[1] = (TextView) findViewById(R.id.headline02);
+        headlines[2] = (TextView) findViewById(R.id.headline03);
+        headlines[3] = (TextView) findViewById(R.id.headline04);
+        headlines[4] = (TextView) findViewById(R.id.headline05);
+
+        int count = 1;
+        for (TextView headline:headlines) {
+            headline.setText("Headline " + count);
+            count++;
+        }
+
+        final TextView[] sources = new TextView[5];
+        sources[0] = (TextView) findViewById(R.id.source01);
+        sources[1] = (TextView) findViewById(R.id.source02);
+        sources[2] = (TextView) findViewById(R.id.source03);
+        sources[3] = (TextView) findViewById(R.id.source04);
+        sources[4] = (TextView) findViewById(R.id.source05);
+
+        count = 1;
+        for (TextView source:sources) {
+            source.setText("Source " + count);
+            count++;
+        }
+
+        final TextView[] dates = new TextView[5];
+        dates[0] = (TextView) findViewById(R.id.datetime01);
+        dates[1] = (TextView) findViewById(R.id.datetime02);
+        dates[2] = (TextView) findViewById(R.id.datetime03);
+        dates[3] = (TextView) findViewById(R.id.datetime04);
+        dates[4] = (TextView) findViewById(R.id.datetime05);
+
+        count = 1;
+        for (TextView date:dates) {
+            date.setText("2018/12/" + count);
+            count++;
+        }
     }
 }
