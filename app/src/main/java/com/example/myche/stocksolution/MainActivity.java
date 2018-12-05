43c06a872912a9165f7098b97465783a80071e69
melissachen2000 <31445526+melissachen2000@users.noreply.github.com>
2018-12-04 19:35:46 -0600

    begin code to receive user input and refresh stocks

--- app/src/main/java/com/example/myche/stocksolution/MainActivity.java
+++ app/src/main/java/com/example/myche/stocksolution/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,6 +45,17 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
+
+        Button newsButton = (Button) findViewById(R.id.button);
+        final EditText userInput = (EditText) findViewById(R.id.editText);
+
+        newsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String maximumStockPrice = userInput.getText().toString().trim();
+                int maxPrice = Integer.parseInt(maximumStockPrice);
+            }
+        });
     }
 
 }
