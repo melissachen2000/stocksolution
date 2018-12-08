75bf8206bc3fbba5bbc71c485ea9a8de4676f4fa
melissachen2000 <31445526+melissachen2000@users.noreply.github.com>
2018-12-08 16:31:08 -0600

    changed so that news page only has three headline cards

--- app/src/main/java/com/example/myche/stocksolution/NewsActivity.java
+++ app/src/main/java/com/example/myche/stocksolution/NewsActivity.java
@@ -20,33 +20,25 @@ public class NewsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.news_page);
 
-        final TextView[] headlines = new TextView[5];
+        final TextView[] headlines = new TextView[3];
         headlines[0] = (TextView) findViewById(R.id.headline01);
         headlines[1] = (TextView) findViewById(R.id.headline02);
         headlines[2] = (TextView) findViewById(R.id.headline03);
-        headlines[3] = (TextView) findViewById(R.id.headline04);
-        headlines[4] = (TextView) findViewById(R.id.headline05);
 
-        final TextView[] sources = new TextView[5];
+        final TextView[] sources = new TextView[3];
         sources[0] = (TextView) findViewById(R.id.source01);
         sources[1] = (TextView) findViewById(R.id.source02);
         sources[2] = (TextView) findViewById(R.id.source03);
-        sources[3] = (TextView) findViewById(R.id.source04);
-        sources[4] = (TextView) findViewById(R.id.source05);
 
-        final TextView[] dates = new TextView[5];
+        final TextView[] dates = new TextView[3];
         dates[0] = (TextView) findViewById(R.id.datetime01);
         dates[1] = (TextView) findViewById(R.id.datetime02);
         dates[2] = (TextView) findViewById(R.id.datetime03);
-        dates[3] = (TextView) findViewById(R.id.datetime04);
-        dates[4] = (TextView) findViewById(R.id.datetime05);
 
-        final Button[] viewArticle = new Button[5];
+        final Button[] viewArticle = new Button[3];
         viewArticle[0] = (Button) findViewById(R.id.article01);
         viewArticle[1] = (Button) findViewById(R.id.article02);
         viewArticle[2] = (Button) findViewById(R.id.article03);
-        viewArticle[3] = (Button) findViewById(R.id.article04);
-        viewArticle[4] = (Button) findViewById(R.id.article05);
 
         for (int i = 0; i < headlines.length; i++) {
             DataRequest headline = new DataRequest(NewsActivity.this, headlines[i]);
@@ -55,15 +47,6 @@ public class NewsActivity extends AppCompatActivity {
             } catch (Exception e) {
                 headlines[i].setText("");
                 viewArticle[i].setText("");
-            }
-        }
-
-        for (int i = 0; i < sources.length; i++) {
-            DataRequest source = new DataRequest(NewsActivity.this, sources[i]);
-            try {
-                source.execute("source", Integer.toString(i), ticker);
-            } catch (Exception e) {
-                sources[i].setText("");
             }
         }
 
@@ -81,16 +64,18 @@ public class NewsActivity extends AppCompatActivity {
             viewArticle[i].setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    DataRequest url = new DataRequest(NewsActivity.this, viewArticle[copyi]);
+                    DataRequest url = new DataRequest(NewsActivity.this, sources[copyi]);
                     try {
                         url.execute("url", Integer.toString(copyi), ticker);
                     } catch (Exception e) { }
 
-                    String link = viewArticle[copyi].getText().toString();
+                    String link = sources[copyi].getText().toString();
 
-                    Intent it = new Intent(Intent.ACTION_VIEW);
-                    it.setData(Uri.parse(link));
-                    startActivity(it);
+                    if (!link.equals("ERROR")) {
+                        Intent it = new Intent(Intent.ACTION_VIEW);
+                        it.setData(Uri.parse(link));
+                        startActivity(it);
+                    }
                 }
             });
 
